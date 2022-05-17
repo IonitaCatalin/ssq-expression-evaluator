36db84a93f7fd1015e34f4633f030580f0bab063
IonitaCatalin <ionita.catalin2000@gmail.com>
2022-05-17 17:43:53 +0300

    added more tests

--- src/test/java/com/evaluator/parser/ParserGetCaseSensitiveTest.java
+++ src/test/java/com/evaluator/parser/ParserGetCaseSensitiveTest.java
@@ -1,4 +1,14 @@
 package com.evaluator.parser;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
 public class ParserGetCaseSensitiveTest {
+
+    @Test
+    public void testGetCaseSensitive() {
+        Parser parser = new Parser();
+        assertFalse(parser.getCaseSensitive());
+    }
 }
